3b1a9855e503941cd7ae8061eb32bdc918d4d88d
Katkamvamsh <katkamvamshisde@gmail.com>
2025-09-09 10:04:00 +0530

    carousel setinterval update 1

--- blocks/carousol/carousol.js
+++ blocks/carousol/carousol.js
@@ -32,5 +32,5 @@ export default function decorate(block) {
     slides[currentIndex].style.display = 'none';
     currentIndex = (currentIndex + 1) % slides.length;
     slides[currentIndex].style.display = 'block';
-  }, 2000);
+  }, 4000);
 }
