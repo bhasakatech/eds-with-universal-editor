42952cdf576faff6aa3812db83468514c597b7ab
nitheesh2929 <nitheesh.r@tychons.com>
2025-09-15 19:28:54 +0530

    footer js updated

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -2,8 +2,9 @@
 export default function decorate(block) {
   console.log('decorate footer', block);
   console.log('block child', block.children);
-  console.log('block child of child', block.children[0].children);
-  console.log('block child of child of child', block.children[0].children[0].children);
+   console.log('block 8thchild', block.children[8]);
+  console.log('block 9thchild of child', block.children[9].children);
+  console.log('block 9thchild of child of child', block.children[9].children[0].children);
   
 
 }
